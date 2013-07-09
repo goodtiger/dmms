a9b924983c25fe235a987e25b27bdc81c4377510
Garfield <Goodtigerzhao@gmail.com>
2013-07-09 20:27:32 +0800

    add "excel-export:0.1.6" plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
-		//compile ":excel-export:0.1.6"
+		compile ":excel-export:0.1.6"
 		
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
